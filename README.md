27d1aaf008017c3640ca6116c2a76b09115d1b88
LeChop <65883366+LeChop@users.noreply.github.com>
2021-05-23 10:41:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Geared-steel
+ 
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+require('boss2-meltdown');
+require('missile-silo');
+require('roads');
+require('gForeshadow');
+require('units');
+
+//planets are now quite working, thanks A LOT to Gdeft for scripts, you rock :)
+require('campaign/zuila');
+require('campaign/serpulo');
+//require('campaign/cori');
+require('campaign/techtree');
